7f90a7bcbac96fdd6fac63d349f2dc7379a3619c
Michael Scofield <mscofield0@tutanota.com>
2022-03-30 15:30:53 +0200

    Added rassert macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,43 @@
+#[macro_export]
+macro_rules! rassert {
+    ($expr: expr, $err: expr) => {
+        let _ = if $expr {
+            Ok(())
+        } else {
+            Err($err)
+        }?;
+    };
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::rassert;
+
+    struct TestError;
+
+    fn driver(f: impl Fn() -> Result<(), TestError>) -> Result<(), TestError> {
+        f()
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn check_ok() {
+        let result = driver(|| {
+            rassert!(1 == 1, TestError);
+
+            Ok(())
+        });
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn check_err() {
+        let result = driver(|| {
+            rassert!(1 != 1, TestError);
+
+            Ok(())
+        });
+
+        assert!(result.is_err());
     }
 }
