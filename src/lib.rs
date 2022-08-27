5dad0246ee2731097fd43a1569deb441bcce253a
Michael Scofield <mscofield0@tutanota.com>
2022-08-27 14:14:06 +0200

    Added rassert_notify for cases where you want to log an error and return from the function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+/// Helper macro to return an Err if the expression fails.
+///
+/// # Example
+/// ```rust
+/// use rassert_rs::rassert;
+///
+/// enum MyError {
+///     SaulBadman,
+/// }
+///
+/// fn test() -> Result<i32, MyError> {
+///     rassert!(42 != 42, MyError::SaulBadman);
+///     Ok(72)
+/// }
+///
+/// fn main() {
+///     assert!(test().is_err());
+/// }
+/// ```
 #[macro_export]
 macro_rules! rassert {
     ($expr: expr, $err: expr) => {
@@ -9,9 +28,30 @@ macro_rules! rassert {
     };
 }
 
+/// Helper macro to cleanly execute an expression and return out of the function if it fails.
+///
+/// # Example
+/// ```rust
+/// use rassert_rs::rassert_notify;
+///
+/// fn main() {
+///     rassert_notify!(42 != 42, println!("Yikes"));
+///     // Prints 'Yikes'
+/// }
+/// ```
+#[macro_export]
+macro_rules! rassert_notify {
+    ($expr: expr, $notify: expr) => {
+        if !$expr {
+            $notify;
+            return;
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::rassert;
+    use crate::{rassert, rassert_notify};
 
     struct TestError;
 
@@ -39,5 +79,13 @@ mod tests {
         });
 
         assert!(result.is_err());
+    }
+
+    #[allow(unused_assignments)]
+    #[test]
+    fn check_notify() {
+        let mut var = 72;
+        rassert_notify!(42 != 42, var = 42);
+        assert_eq!(var, 42);
     }
 }
